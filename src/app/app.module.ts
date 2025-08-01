fdc8f6391c15db4dbf70167d357c5f477bd6d80c
Trần Ngọc Phú <phu.tran@lifesup.local>
2025-08-01 17:15:11 +0700

    new-design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    DetailProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
