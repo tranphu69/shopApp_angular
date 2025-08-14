376891a6c4bf6115b1255f919320eb29ff8718e0
Trần Ngọc Phú <phu.tran@lifesup.local>
2025-08-14 14:31:04 +0700

    login-register

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, retry } from 'rxjs';
+import { RegisterDTO } from '../dtos/user/register.dto';
+import { LoginDTO } from '../dtos/user/login.dto';
+import { environment } from 'src/app/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiRegister = `${environment.apiBaseUrl}/users/register`;
+  private apiLogin = `${environment.apiBaseUrl}/users/login`;
+  private apiConfig = {
+    headers: this.createHeaders()
+  }
+
+  constructor(private http: HttpClient) {}
+
+  private createHeaders(): HttpHeaders {
+    return new HttpHeaders({ 'Content-Type': 'application/json'});
+  }
+
+  register(registerDTO: RegisterDTO):Observable<any> {
+    return this.http.post(this.apiRegister, registerDTO, this.apiConfig);
+  }
+
+  login(loginDTO: LoginDTO):Observable<any> {
+    return this.http.post(this.apiLogin, loginDTO, this.apiConfig);
+  }
+}
